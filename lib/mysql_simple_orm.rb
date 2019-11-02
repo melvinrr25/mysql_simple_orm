356669335ab596b5e71489fe6a151a090e0c0367
Melvin Rodriguez <melvinrr25@gmail.com>
2019-11-01 22:56:34 -0600

    Initial commmit

---
+++ lib/mysql_simple_orm.rb
@@ -0,0 +1,5 @@
+require "mysql_simple_orm/version"
+
+module MysqlSimpleOrm
+  # Your code goes here...
+end
---
+++ spec/mysql_simple_orm_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MysqlSimpleOrm do
+  it "has a version number" do
+    expect(MysqlSimpleOrm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
